026f67c59c4af812d62ae642c4e941928ba70070
khe4oyan <khe4oyan@gmail.com>
2024-04-24 19:46:01 +0400

    .

--- frontend/src/components/PassTest/PassTest.js
+++ frontend/src/components/PassTest/PassTest.js
@@ -1,43 +1,44 @@
-import {useParams} from 'react-router-dom';
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-
+import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function PassTest() {
-	const testId = useParams();
-	const [test, setTest] = useState(null);
+  const testId = useParams();
+  const [test, setTest] = useState(null);
 
-	useEffect(() => {
+  useEffect(() => {
     const fetchTest = async () => {
       try {
-        const response = await axios.getget(`http://localhost:5087/api/testById?testId=${testId}`);
+        const response = await axios.getget(
+          `http://localhost:5087/api/testById?testId=${testId}`
+        );
         setTest(response.data);
       } catch (error) {
-        console.error('Error fetching test:', error);
+        console.error("Error fetching test:", error);
       }
     };
 
     fetchTest();
   }, [testId]);
 
-	return (
-		<div className="formTitleContainer">
-          <div>
-            <input
-              className="formName"
-              type="text"
-              placeholder="Test Name"
-               value={test?.name}
-              // onChange={handleInputChange} 
-            ></input>
-          </div>
-          <input
-            className="formDescription"
-            type="text"
-            placeholder="Test Description..."
-          ></input>
+  return (
+    <div className="formTitleContainer">
+      <div>
+        <input
+          className="formName"
+          type="text"
+          placeholder="Test Name"
+          value={test?.name}
+          // onChange={handleInputChange}
+        ></input>
+      </div>
+      <input
+        className="formDescription"
+        type="text"
+        placeholder="Test Description..."
+      ></input>
     </div>
-	)
+  );
 }
 
 export default PassTest;
--- frontend/src/components/TeacherAccount/YourTests.js
+++ frontend/src/components/TeacherAccount/YourTests.js
@@ -1,40 +1,71 @@
-import React from 'react';
+import React from "react";
 import "./YourTests.css";
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
-import { faPlus,} from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
 
 function YourTests({ teacherId }) {
   const [teacherTests, setTeacherTests] = useState([]);
 
+  const [grouptTestId, setGroupTestId] = useState(-1);
+  const [isShowedGroupes, setIsShowedGroups] = useState(false);
+  const [groupNames, setGroupNames] = useState([]);
+  const [selectedGroupes, setSelectedGroups] = useState([]);
+
+
   async function fetchTestsByTeacherId(teacherId) {
     try {
-      const response = await axios.get(`http://localhost:5087/api/tests?teacherId=${teacherId}`);
-      setTeacherTests(response.data); 
+      const response = await axios.get(
+        `http://localhost:5087/api/tests?teacherId=${teacherId}`
+      );
+      setTeacherTests(response.data);
     } catch (error) {
-      console.error('Error fetching tests:', error);
-      setTeacherTests([]); 
+      console.error("Error fetching tests:", error);
+      setTeacherTests([]);
     }
   }
 
   useEffect(() => {
-    fetchTestsByTeacherId(teacherId); 
-  }, []); 
+    fetchTestsByTeacherId(teacherId);
 
-  const handleClick = (test) => {
-    console.log('Clicked test:', test);
+    // yani fetch arinq grouperi cucaky
+    // TODO: zapros aneluc set ara sran grouperi anunnery: (setGroupNames(array))
+    setGroupNames([
+      "group 1",
+      "group 2",
+      "group 3",
+    ]);
+  }, []);
+
+  // const handleClick = (test) => {
+  //   console.log("Clicked test:", test);
+  // };
+
+  const handleAddGroup = (testId, testIndex) => {
+    setGroupTestId(testId);
+
+    setSelectedGroups(prev => [...prev, groupNames[testIndex]]);
+    setGroupNames(prev => prev.filter((_, i) => i !== testIndex));
   };
 
+  const removeGroupNameByIndex = (testIndex) => {
+    setGroupNames(prev => [...prev, selectedGroupes[testIndex]]);
+    setSelectedGroups(prev => prev.filter((_, i) => i !== testIndex));
+  }
+
+  const groupsDoneButton = () => {
+    setIsShowedGroups(false);
+    // TODO: uxarkel server: selectedGroupes <-- zangvac
+  }
 
   console.log(teacherTests);
   return (
     <div id="yourTests" className="yourTestsContainer">
       <h1>Your Tests</h1>
       <br></br>
-      <table className='testsTable'>
+      <table className="testsTable">
         <thead>
           <tr>
             <th>Test Name</th>
@@ -42,24 +73,55 @@ function YourTests({ teacherId }) {
             <th>Groups To</th>
           </tr>
         </thead>
-          <tbody>
-            {teacherTests.map(test => (
-              <tr key={test._id}>
-                <td ><Link to={`/pass/test/${test._id}`}>{test.name}</Link></td>
-                <td>{test.subject}</td>
-                <td>{test.groups}
-                <FontAwesomeIcon icon={faPlus} 
-                // onClick={handleAddGroup(test._id)
-                >
-                </FontAwesomeIcon>
-                </td> 
-              </tr>
-            ))}
-          </tbody>
+        <tbody>
+          {teacherTests.map((test) => (
+            <tr key={test._id}>
+              <td>
+                <Link to={`/pass/test/${test._id}`}>{test.name}</Link>
+              </td>
+              <td>{test.subject}</td>
+              <td>
+                {test.groups}
+                {isShowedGroupes ? (
+                  <div>
+                    {selectedGroupes.map((groupname, i) => (
+                      <div key={i}>
+                        <p>{groupname}</p>
+                        <button onClick={() => removeGroupNameByIndex(i)}>x</button>
+                      </div>
+                    ))}
+
+                    {groupNames.map((groupName, i) => (
+                      <button key={i} onClick={() => handleAddGroup(test.id, i)}>
+                        {groupName}
+                      </button>
+                    ))}
+
+                    <button onClick={groupsDoneButton}>done</button>
+                  </div>
+                ) : (
+                  <>
+                    {selectedGroupes.map((groupname, i) => (
+                      <div key={i}>
+                        <p>{groupname}</p>
+                      </div>
+                    ))}
+                    <button
+                      onClick={() => {
+                        setIsShowedGroups(true);
+                      }}
+                    >
+                      <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+                    </button>
+                  </>
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
 }
-
 
 export default YourTests;
